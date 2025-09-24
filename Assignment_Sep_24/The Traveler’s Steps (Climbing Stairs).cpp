9584b7b5df283c62a981e694a134bb3142def06a
Sumit-Kumar-Dubey-179 <sumitdubey179@gmail.com>
2025-09-24 23:20:21 +0530

    Update The Traveler’s Steps (Climbing Stairs).cpp
    Necessary Change

--- Assignment_Sep_24/The Traveler’s Steps (Climbing Stairs).cpp
+++ Assignment_Sep_24/The Traveler’s Steps (Climbing Stairs).cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int climbStairs(int n) {
-    if (n == 0) return 1;  // reached top
-    if (n < 0) return 0;   // invalid path
-    return climbStairs(n - 1) + climbStairs(n - 2);
+int sumit(int n) {
+    if (n == 0) 
+    return 1;  
+    if (n < 0)
+    return 0;  
+    return (sumit(n - 1) + sumit(n - 2));
 }
 
 int main() {
     int n;
     cin >> n;
-    cout << climbStairs(n) << endl;
+    cout << sumit(n) << endl;
     return 0;
 }
